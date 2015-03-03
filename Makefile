94297c64fc0d6ab45d43fcf7afa078a63ecffbff
Bertrand Gouny <bgouny@hi-media.com>
2015-03-03 15:34:01 +0100

    tests

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 NAME = osixia/wordpress
-VERSION = 0.1.1
+VERSION = 0.1.2
 
 .PHONY: all build test tag_latest release
 
--- image/service/wordpress/container-start.sh
+++ image/service/wordpress/container-start.sh
@@ -6,18 +6,6 @@ FIRST_START_DONE="/etc/docker-wordpress-first-start-done"
 # container first start
 if [ ! -e "$FIRST_START_DONE" ]; then
 
-  # wordpress directory is empty, we use the bootstrap
-  if [ ! "$(ls -A /var/www/wordpress)" ]; then
-    mkdir -p /var/www/wordpress/sources
-    cp -R /var/www/wordpress_bootstrap/* /var/www/wordpress/sources
-    rm -rf /var/www/wordpress_bootstrap
-
-    # Move wp-config.php 
-    mv /var/www/wordpress/sources/wp-config-sample.php /var/www/wordpress/wp-config.php
-  fi  
-
-
-
   # create wordpress vhost
   if [ "${HTTPS,,}" == "true" ]; then
 
@@ -35,29 +23,40 @@ if [ ! -e "$FIRST_START_DONE" ]; then
     a2ensite wordpress
   fi
 
-  # set new install default theme
-  if [ -n "$WORDPRESS_DEFAULT_THEME" ]; then
-    sed -i "s/define( 'WP_DEFAULT_THEME', '[^']*'/define( 'WP_DEFAULT_THEME', '${WORDPRESS_DEFAULT_THEME}'/g" /var/www/wordpress/sources/wp-includes/default-constants.php
-  fi
+  # wordpress directory is empty, we use the bootstrap
+  if [ ! "$(ls -A /var/www/wordpress)" ]; then
+    mkdir -p /var/www/wordpress/sources
+    cp -R /var/www/wordpress_bootstrap/* /var/www/wordpress/sources
+    rm -rf /var/www/wordpress_bootstrap
 
-  # set db configuration
-  sed -i "s/define('DB_NAME', '[^']*'/define('DB_NAME', '${DB_NAME}'/g" /var/www/wordpress/wp-config.php
-  sed -i "s/define('DB_USER', '[^']*'/define('DB_USER', '${DB_USER}'/g" /var/www/wordpress/wp-config.php
-  sed -i "s/define('DB_PASSWORD', '[^']*'/define('DB_PASSWORD', '${DB_PASSWORD}'/g" /var/www/wordpress/wp-config.php
-  sed -i "s/define('DB_HOST', '[^']*'/define('DB_HOST', '${DB_HOST}'/g" /var/www/wordpress/wp-config.php
-  sed -i "s/$table_prefix  = '[^']*'/$table_prefix  = '${DB_TABLE_PREFIX}'/g" /var/www/wordpress/wp-config.php
+    # Move wp-config.php 
+    mv /var/www/wordpress/sources/wp-config-sample.php /var/www/wordpress/wp-config.php
 
-  # set authentication unique keys and salts.
-  get_salt () {
-    salt=$(</dev/urandom tr -dc '1324567890#<>,()*.^@$% =-_~;:|{}[]+!`azertyuiopqsdfghjklmwxcvbnAZERTYUIOPQSDFGHJKLMWXCVBN' | head -c64 | tr -d '\\')
-  }
+    # set new install default theme
+    if [ -n "$WORDPRESS_DEFAULT_THEME" ]; then
+      sed -i "s/define( 'WP_DEFAULT_THEME', '[^']*'/define( 'WP_DEFAULT_THEME', '${WORDPRESS_DEFAULT_THEME}'/g" /var/www/wordpress/sources/wp-includes/default-constants.php
+    fi
 
-  toSalt=( AUTH_KEY SECURE_AUTH_KEY LOGGED_IN_KEY NONCE_KEY AUTH_SALT SECURE_AUTH_SALT LOGGED_IN_SALT NONCE_SALT )
-  for key in ${toSalt[*]}
-  do
-    get_salt
-    sed -i "s/define('${key}', *'[^']*'/define('${key}', '${salt}'/g" /var/www/wordpress/wp-config.php 
-  done
+    # set db configuration
+    sed -i "s/define('DB_NAME', '[^']*'/define('DB_NAME', '${DB_NAME}'/g" /var/www/wordpress/wp-config.php
+    sed -i "s/define('DB_USER', '[^']*'/define('DB_USER', '${DB_USER}'/g" /var/www/wordpress/wp-config.php
+    sed -i "s/define('DB_PASSWORD', '[^']*'/define('DB_PASSWORD', '${DB_PASSWORD}'/g" /var/www/wordpress/wp-config.php
+    sed -i "s/define('DB_HOST', '[^']*'/define('DB_HOST', '${DB_HOST}'/g" /var/www/wordpress/wp-config.php
+    sed -i "s/$table_prefix  = '[^']*'/$table_prefix  = '${DB_TABLE_PREFIX}'/g" /var/www/wordpress/wp-config.php
+
+    # set authentication unique keys and salts.
+    get_salt () {
+      salt=$(</dev/urandom tr -dc '1324567890#<>,()*.^@$% =-_~;:|{}[]+!`azertyuiopqsdfghjklmwxcvbnAZERTYUIOPQSDFGHJKLMWXCVBN' | head -c64 | tr -d '\\')
+    }
+
+    toSalt=( AUTH_KEY SECURE_AUTH_KEY LOGGED_IN_KEY NONCE_KEY AUTH_SALT SECURE_AUTH_SALT LOGGED_IN_SALT NONCE_SALT )
+    for key in ${toSalt[*]}
+    do
+      get_salt
+      sed -i "s/define('${key}', *'[^']*'/define('${key}', '${salt}'/g" /var/www/wordpress/wp-config.php 
+    done
+
+  fi 
 
   # Fix file permission
   find /var/www/ -type d -exec chmod 755 {} \;
